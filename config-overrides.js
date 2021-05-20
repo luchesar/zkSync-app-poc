730e9585adf2f8d7c38b6ef9652beee9b3fb2698
Lucho <luchesar@gmail.com>
2021-05-20 19:17:00 +0300

    Enable webpack devServer CORS headers

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,31 @@
 // config-overrides.js
-module.exports = function override(config, env) {
+module.exports = {
     // New config, e.g. config.plugins.push...
 
-    console.log("Overriding config...", config);
+    devServer: function(configFunction) {
+        // Return the replacement function for create-react-app to use to generate the Webpack
+        // Development Server config. "configFunction" is the function that would normally have
+        // been used to generate the Webpack Development server config - you can use it to create
+        // a starting configuration to then modify instead of having to create a config from scratch.
+        return function(proxy, allowedHost) {
+        // Create the default config by calling configFunction with the proxy/allowedHost parameters
+            const config = configFunction(proxy, allowedHost);
 
-    const devServer = config.devServer || {};
-    const headers = devServer.headers || {};
+            console.log("Overriding config...", config);
 
-    headers["Access-Control-Allow-Origin"] = "*";
-    headers["Access-Control-Allow-Credentials"] = "true";
-    headers["Access-Control-Allow-Methods"] = "POST, PUT, GET, OPTIONS, DELETE";
-    headers["Access-Control-Allow-Headers"] = "Content-Type, Accept, X-Requested-With, Authorization";
-    headers["Access-Control-Max-Age"] = "360000";
+            const headers = config.headers || {};
 
-    devServer.headers = headers;
-    config.devServer = devServer;
-    console.log("Config...", config);
+            headers["Access-Control-Allow-Origin"] = "*";
+            headers["Access-Control-Allow-Credentials"] = "true";
+            headers["Access-Control-Allow-Methods"] = "POST, PUT, GET, OPTIONS, DELETE";
+            headers["Access-Control-Allow-Headers"] = "Content-Type, Accept, X-Requested-With, Authorization";
+            headers["Access-Control-Max-Age"] = "359998";
 
-    return config
+            config.headers = headers;
+            console.log("Config...", config);
+
+            // Return your customised Webpack Development Server config.
+            return config;
+        };
+    }
 }
